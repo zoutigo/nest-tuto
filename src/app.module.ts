55f2da5dd9cdd2ab138dbef57ef65866214311a2
zoutigo <zoutigo@gmail.com>
2022-06-29 10:37:52 +0200

    connecting to mysql using type orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CostumersController } from './costumers/controllers/costumers/costumers.controller';
 import { CostumersModule } from './costumers/costumers.module';
 import { ValidateBehaviourMiddleware } from './globalMiddlewares/validate-behaviour.middleware';
 import { HousesModule } from './houses/houses.module';
+import entities from './typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CostumersModule, HousesModule, UsersModule],
+  imports: [
+    CostumersModule,
+    HousesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'zoutigo',
+      password: 'valery54',
+      database: 'nest_tutorial_db',
+      entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
